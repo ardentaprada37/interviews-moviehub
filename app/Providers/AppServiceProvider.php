0b6dbaad877c84bd1f97a5183a267b5fe6f753c1
rens12c <ardentanirvana12@gmail.com>
2026-02-21 11:19:15 +0700

    Force HTTPS in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        // Force HTTPS in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Auth::provider('custom', function ($app, array $config) {
             return new \Illuminate\Auth\EloquentUserProvider($app['hash'], $config['model']);
         });
